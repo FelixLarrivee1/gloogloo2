2434c251adf95d336e09c9d585a1143cbb9a97d9
FelixLarrivee2 <felix.larrivee.2@ulaval.ca>
2025-09-03 11:42:08 -0400

    asd

--- test.java
+++ test.java
@@ -1,2 +1,7 @@
-package org.gloogloo2;public class test {
+package org.gloogloo2;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
